2f3762408d816a91493dfcf5d5bc2fdc96e7e75a
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2022-03-24 15:24:33 -0300

    add new url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  url: 'https://api.github.com/search/issues?q=repo:frontendbr/vagas+type:issue+state:open',
-  apiKey: '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'
+  url: 'https://api.github.com/search/issues',
+  apiKey: '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',
 };
